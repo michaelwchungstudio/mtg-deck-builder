d89eefe96b631e9b0c78e89634eec3c76fa3f1d9
Manny <devolthdd@gmail.com>
2018-11-29 17:13:30 -0500

    End of day git committer

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,11 +1,22 @@
 class DecksController < ApplicationController
+  $set = MTG::Set.all
   def index
+    @user = current_user
+    @decks = Deck.where(user_id: @user.id)
   end
 
   def new
+    @deck = Deck.new
   end
 
   def create
+    @user = current_user
+    deck = Deck.new(deck_params)
+    if deck.save
+      redirect_to "/"
+    else 
+      redirect_to "/users/#{@user.id}/decks/new"
+    end
   end
 
   def show
@@ -18,6 +29,11 @@ class DecksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def deck_params
+    params.require(:deck).permit(:name, :cardlist, :user_id)
   end
 
 
