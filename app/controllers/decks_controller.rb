10c2e2a4ee44ed7711c28ef84cf4f7254bb4592e
Manny <devolthdd@gmail.com>
2018-12-01 21:29:55 -0500

    Testing and sving for a pull

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -26,77 +26,28 @@ class DecksController < ApplicationController
   def edit
     p params
     @deck = Deck.where(id: params[:id]).first
+    @user = User.where(id: params[:user_id]).first
+    p params[:page_num]
+    @pagenum = params[:page_num]
+    @cards = MTG::Card.where(page: params[:page_num]).where(pageSize: 9).all
   end
 
   def search
-    @deck = Deck.where(id: params[:id])
-    @user = User.where(id: params[:user_id])
-            @card = MTG::Card.where(name: params[:card_name]).where(colors: params[:card_color]).where(type: params[:card_type]).where(subtype: params[:creature_type]).all
-            p @card
-    
-    if(params[:card_name] != "")
-      if(params[:card_color] != "")
-        if (params[:card_type] != "")
-          if (params[:creature_type] != "")
-            if(params[:set] != "")
-
-            #else
-
-            end
-          #else
-
-          end
-
-        #else
-          
-        end
-
-      #else
-
-      end
-
-    #else
-      # if(params[:card_color] != "")
-      #   if (params[:card_type] != "")
-      #     if (params[:creature_type] != "")
-              # if (params[:set] != "")
-
-              # else
-                
-              # end
-
-      #     else
-
-      #     end
-
-      #   else
-          
-      #   end
-
-      # else
-
-      # end
+    @deck = Deck.where(id: params[:deck_id]).first
+    @user = User.where(id: params[:user_id]).first
+    if(params[:card_name] == "" && params[:card_color] == "" && params[:card_type] == "" && params[:creature_type] == "" && params[:set] == "")
+      @cards = MTG::Card.where(page: 1).where(pageSize: 9).all
+      redirect_to "/users/#{ @user.id }/decks/#{ @deck.id }/edit/1"
+    else
+      @cards = MTG::Card.where(name: params[:card_name]).where(colors: params[:card_color]).where(type: params[:card_type]).where(subtype: params[:creature_type]).where(set: params[:set]).all
+      redirect_to "/users/#{ @user.id }/decks/#{ @deck.id }/result"
     end
 
-    #if(params[:name] != "" && params[:card_color] == "" && params[:card_type] && params[:creature_type] == "" && params[:set] == "")
-
-
-    #   @card = MTG::Card.where(name: params[:name])
-    #   if(@card.length == 0)
-    #     @card = "None found"
-    #   end
-    # elsif(params[:name] != "" && params[:card_color] != "" && params[:creature_type] == "")
-    #   temp = MTG::Card.where(name: params[:name]).where(colors: params[:card_color])
-    #   @card = []
-    #   if temp.length != 0
-    #     @card = temp
-    #   else
-    #     @card = "None found"
-    #   end 
-    # elsif( params[:name] != "" && params[:card_color] == "" && params[:creature_type] != "")
-    #   temp = MTG::Card.where(name: )
-    
-     #end
+    #redirect_back(fallback_location: root_path)
+  end
+  def result
+    @deck = Deck.where(id: params[:id]).first
+    @user = User.where(id: params[:user_id]).first
   end
 
   def update
@@ -109,6 +60,8 @@ class DecksController < ApplicationController
   def deck_params
     params.require(:deck).permit(:name, :cardlist, :user_id)
   end
-
+  def deck_params2
+    params.require(:deck).permit(:user_id, :deck_id, :page_num)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get "/users" => "users#index"
   get "/profile/:id" => "users#profile", :as => :user_profile
+  get "/users/:user_id/decks/:id/edit/:page_name" => "decks#edit"
+  get "/users/:user_id/decks/:id/result" => "decks#result"
   post "/search" => "decks#search"
 
   resources :users do
