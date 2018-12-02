afd16ff1fd1085f4a0715e6b9c547bb03b467c97
Michael W Chung <dryhslaw@gmail.com>
2018-12-01 20:06:13 -0500

    show deck page added hover functionality and hash of decklist

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,16 +1,35 @@
 class DecksController < ApplicationController
 
+  # ? - Manny's variable (?)
   $set = MTG::Set.all
-  
+
+  # List of all decks with the corresponding user creator
+  # Clicking a deck leads to that deck's show page
+  # Clicking a user leads to that user's profile page of his/her decks
   def index
     @user = current_user
     @decks = Deck.where(user_id: @user.id)
   end
 
+  # Create a new deck - text input with a deck name
+  # Leads to deck edit page
   def new
     @deck = Deck.new
   end
 
+  # POST action - saves the deck with input name
+  def create
+    @user = current_user
+    deck = Deck.new(deck_params)
+    if deck.save
+      redirect_to "/users/#{@user.id}/decks/#{deck.id}/edit/1"
+    else
+      redirect_to "/users/#{@user.id}/decks/new"
+    end
+  end
+
+  # Custom POST action called upon clicking a card's 'add to deck' button
+  # Retrieves the card_id through a hidden field and adds it to the deck's string property 'cardlist'
   def addCardToDeck
     @user = current_user
     card_id = params[:card_id]
@@ -23,19 +42,6 @@ class DecksController < ApplicationController
     @deck.save
 
     redirect_to "/users/#{@user.id}/decks/#{@deck.id}/edit/1"
-  end
-
-  def create
-    @user = current_user
-    deck = Deck.new(deck_params)
-    if deck.save
-      redirect_to "/users/#{@user.id}/decks/#{deck.id}/edit/1"
-    else
-      redirect_to "/users/#{@user.id}/decks/new"
-    end
-  end
-
-  def show
   end
 
   def edit
@@ -80,6 +86,41 @@ class DecksController < ApplicationController
       end
 
     end
+  end
+
+  # Show a specific deck's cards
+  def show
+    require 'set'
+
+    @deck = Deck.find(params[:id])
+
+    # this repeats from edit action - perhaps create custom func (needs more DRY)
+    @deck_card_names = []
+    @deck_card_images = []
+
+    deck_card_list_array = @deck.cardlist.split(",")
+    @card_list_hash = Hash.new(0)
+
+    deck_card_list_array.each do |card_id|
+      if specific_card = MTG::Card.find(card_id)
+        @deck_card_names.push(specific_card.name)
+        @deck_card_images.push(specific_card.image_url)
+      end
+    end
+
+    @image_set = Set.new(@deck_card_images)
+    @image_set_string = ""
+
+    @deck_card_names.each do |cardname|
+      @card_list_hash[cardname] += 1
+    end
+
+    @image_set.each do |imagelink|
+      @image_set_string += (imagelink + ",")
+    end
+
+    p @image_set_string
+
   end
 
   def update
