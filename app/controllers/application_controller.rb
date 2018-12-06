698e3c7caf98b9399d1a3a650d77be1659a0356f
Michael W Chung <dryhslaw@gmail.com>
2018-12-06 15:23:26 -0500

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   MTG.configure do |config|
     config.api_version = 1
   end
+
+  def after_sign_in_path_for(resource)
+    user_profile_path(current_user)
+  end
 end
